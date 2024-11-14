9568806a19628988e3e00525599a581feff1d57c
gilbert-ku <gilbert45ku@gmail.com>
2024-11-14 16:48:25 +0300

    create an asset file and and a logo in splash page

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -21,10 +21,26 @@ class _SplashPageState extends State<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: "Gchat",
       theme: ThemeData(
         dialogBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
         scaffoldBackgroundColor: const Color.fromRGBO(36, 35, 49, 1.0),
+      ),
+      home: Scaffold(
+        body: Center(
+          child: Container(
+            height: 200,
+            width: 200,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  fit: BoxFit.contain,
+                  image: AssetImage(
+                    "assets/images/backround_image.png",
+                  )),
+            ),
+          ),
+        ),
       ),
     );
   }
